59057bf332ae6ecf9ee41950b3169f4c03a174fa
rohendi <hendylucky@gmail.com>
2017-03-31 21:48:49 +0700

    Add files via upload

---
+++ lth2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int max (int a,int b)
+
+{
+if (a>b) {
+return a ;
+}
+else {
+return b ;
+}
+}
+int main () {
+cout << "hasilnya adalah: "<< max (7,8);
+return 0 ;
+
+}
